b372a690110c5f23aa0348b1473efb96c1db4842
Krista Korento <krista.korento@gmail.com>
2024-10-27 16:54:18 +0200

    Changed app.jsx file name.

--- my-school-react-project/src/main.jsx
+++ my-school-react-project/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './AppMain.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
